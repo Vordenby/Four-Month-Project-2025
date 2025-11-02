ed9afe705e78af88b754813e784276cc3b2d0ddf
vordenby <ceo65v@gmail.com>
2025-11-03 01:28:38 +0300

    tablecreator

---
+++ Table.py
@@ -0,0 +1,35 @@
+class Table():
+
+    def __init__(self, data):
+        self.data = data
+
+    
+    def CreateTable(self):
+
+        def GetPath():
+            
+            import os, sys
+
+            if getattr(sys, 'frozen', False):
+                os.chdir(os.path.dirname(sys.executable))
+            else:
+                os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        
+        path = GetPath()
+
+        Xes = [], Yes = []
+
+        for i in self.data:
+            Xes.append(i[0])
+            Yes.append(i[1])
+
+        import pandas as pd
+
+        Data_Frame = pd.DataFrame(
+            {
+                "x": [x for x in Xes],
+                "y": [y for y in Yes]
+            }
+        )
+
+        Data_Frame.to_csv("Function_" + str(random.randint(1, 20000)))
